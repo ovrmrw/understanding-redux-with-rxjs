e9a30c538a65a5df59edc5be8e7fc7266fe632a8
ovrmrw <t.ngc.0403@gmail.com>
2016-09-22 14:10:21 +0900

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # understanding-redux-with-rxjs
 Understanding Redux concept with RxJS. This is an easy example to make sense for newbies.
+
+---
+
+|Compiler|version|
+|:--|:--|
+|typescript|2.0.2|
+|babel-preset-latest|6.14.0|
+
+||My Environments|
+|:--|:--|
+|OS|Windows 10 Pro 64bit or 7 Pro 32bit|
+|Editor|VSCode (stable)|
+|node --version|v6.3.1|
+
+### Setup
+```
+$ npm install
+```
+
+### Run (ts files directly)
+```
+$ npm run ts
+or
+$ npm run ts:alt
+```
+
+### Run (js files after build)
+```
+$ npm start
+```
---
+++ redux-rxjs.ts
@@ -0,0 +1,79 @@
+import 'babel-polyfill';
+import 'zone.js/dist/zone-node';
+import { Observable, Subject, BehaviorSubject } from 'rxjs/Rx';
+declare const Zone: any;
+
+
+class IncrementAction {
+  constructor(public num: number) { }
+}
+
+type Action = IncrementAction | null;
+
+interface IncrementState {
+  counter: number;
+}
+
+interface AppState {
+  increment: Promise<IncrementState>
+}
+
+
+Zone.current.fork({ name: 'myZone' }).run(() => {
+
+  console.log('zone name:', Zone.current.name); /* OUTPUT> zone name: myZone */
+
+  const appState: AppState = {
+    increment: Promise.resolve({
+      counter: 0
+    })
+  };
+
+  let counter: number;
+
+
+  const dispatcher$ = new Subject<Action>();
+  const provider$ = new BehaviorSubject<AppState>(appState);
+
+
+  Observable // ReducerContainer
+    .zip<AppState>(...[
+      dispatcher$.scan<Promise<IncrementState>>((state, action) => {
+        if (action instanceof IncrementAction) {
+          return new Promise<IncrementState>(resolve => {
+            setTimeout(() => {
+              state.then(s => {
+                resolve({ counter: s.counter + action.num });
+              });
+            }, 10);
+          });
+        } else {
+          return state;
+        }
+      }, appState.increment),
+      (increment) => {
+        return { increment } as AppState
+      }
+    ])
+    .subscribe(appState => {
+      provider$.next(appState);
+    });
+
+
+  provider$
+    .map<Promise<IncrementState>>(appState => appState.increment)
+    .mergeMap<IncrementState>(state => {
+      return Observable.fromPromise(state);
+    })
+    .distinctUntilChanged()
+    .subscribe(state => {
+      counter = state.counter;
+      console.log('counter:', counter); /* (First time) OUTPUT> counter: 0 */
+    });
+
+
+  dispatcher$.next(new IncrementAction(1)); /* OUTPUT> counter: 1 */
+  dispatcher$.next(new IncrementAction(1)); /* OUTPUT> counter: 2 */
+  dispatcher$.next(new IncrementAction(1)); /* OUTPUT> counter: 3 */
+
+});
